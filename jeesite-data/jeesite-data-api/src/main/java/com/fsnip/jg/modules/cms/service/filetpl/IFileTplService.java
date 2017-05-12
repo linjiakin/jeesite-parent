9b6c4f51afa8bde409d9adb8b3e32b4981619e67
tanfangyong <tanfanyong@fsnip.com>
2017-05-12 17:00:08 +0800

    对jeesite 进行架构迁移 转换为分布式系统架构 Dubbo +Zookeeper

---
+++ jeesite-data/jeesite-data-api/src/main/java/com/fsnip/jg/modules/cms/service/filetpl/IFileTplService.java
@@ -0,0 +1,7 @@
+package com.fsnip.jg.modules.cms.service.filetpl;
+
+/**
+ * Created by tanfy on 2017/5/12 0012.
+ */
+public interface IFileTplService {
+}
---
+++ jeesite-data/jeesite-data-api/src/main/java/com/fsnip/jg/modules/sys/service/ILogService.java
@@ -0,0 +1,11 @@
+package com.fsnip.jg.modules.sys.service;
+
+import com.fsnip.jg.common.persistence.Page;
+import com.fsnip.jg.modules.sys.entity.Log;
+
+/**
+ * Created by tanfy on 2017/4/19 0019.
+ */
+public interface ILogService {
+    public Page<Log> findPage(Page<Log> page, Log log);
+}
